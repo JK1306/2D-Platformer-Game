b5297b007ede96b044ea2a5b70bbd46108889c6f
Jaikishore <jaikishore1997@gmail.com>
2021-10-16 16:12:21 +0530

    Add : Jump animation using Scripting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Animator playerAnimator;
+    private float speed, jump;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,23 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 playerVector = transform.localScale;
+
+        speed = Input.GetAxis("Horizontal");
+        jump = Input.GetAxis("Vertical");
+
+        playerAnimator.SetFloat("speed",Mathf.Abs(speed));
+        if(speed < 0){
+            playerVector.x = -1f * Mathf.Abs(playerVector.x);
+        }else if(speed > 0){
+            playerVector.x = Mathf.Abs(playerVector.x);
+        }
+        transform.localScale = playerVector;
+
+        if(0<jump){
+            playerAnimator.SetBool("jump",true);
+        }else{
+            playerAnimator.SetBool("jump",false);
+        }
     }
 }
