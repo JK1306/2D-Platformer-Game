eb6912a426a2e1a450d0eeb390112ef09630d8dd
Jaikishore <jaikishore1997@gmail.com>
2021-10-19 00:52:25 +0530

    Add : Player Run and Jump movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed, jump;
     public Animator playerAnimator;
-    BoxCollider2D boxCollider;
-    private float speed, jump;
     float offsetY, boxY;
+    bool inAir = false;
+    Rigidbody2D rb2d;
+    BoxCollider2D boxCollider;
+    private float horizontalForce, verticalForce;
     // Start is called before the first frame update
     void Start()
     {
         boxCollider = gameObject.GetComponent<BoxCollider2D>();
-        // Debug.Log(boxCollider.center);
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
         boxY = boxCollider.size.y;
         offsetY = boxCollider.offset.y;
     }
@@ -20,35 +23,74 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalForce = Input.GetAxis("Horizontal");
+        verticalForce = Input.GetAxis("Jump");
+
+        PlayerAnimation();
+        PlayerMovement();
+    }
+
+    public void PlayerMovement(){
+        Vector3 playerPosition = transform.localPosition;
+        if(horizontalForce != 0){
+            playerPosition.x += speed * horizontalForce * Time.deltaTime;
+            transform.localPosition = playerPosition;
+        }
+        if((verticalForce > 0) && !(inAir)){
+            Debug.Log("Force has been applied");
+            rb2d.AddForce(new Vector2(0,jump), ForceMode2D.Force);
+            inAir = true;
+        }
+
+    }
+
+    private void PlayerAnimation()
+    {
         Vector3 playerVector = transform.localScale;
 
-        speed = Input.GetAxis("Horizontal");
-        jump = Input.GetAxis("Jump");
-        playerAnimator.SetFloat("speed",Mathf.Abs(speed));
-        if(speed < 0){
+        playerAnimator.SetFloat("speed", Mathf.Abs(horizontalForce));
+        if (horizontalForce < 0)
+        {
             playerVector.x = -1f * Mathf.Abs(playerVector.x);
-        }else if(speed > 0){
+        }
+        else if (horizontalForce > 0)
+        {
             playerVector.x = Mathf.Abs(playerVector.x);
         }
         transform.localScale = playerVector;
 
-        if(0<jump){
-            playerAnimator.SetBool("jump",true);
-        }else{
-            playerAnimator.SetBool("jump",false);
+        // jump
+        if (0 < verticalForce)
+        {
+            if(!inAir)
+            playerAnimator.SetBool("jump", true);
+        }
+        else if(!inAir)
+        {
+            playerAnimator.SetBool("jump", false);
         }
 
-        if(Input.GetKeyDown(KeyCode.LeftControl)){
-            playerAnimator.SetBool("crouch",true);
+        // crouch
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            playerAnimator.SetBool("crouch", true);
             // Debug.Log("BoxY Value :"+boxY);
             // Debug.Log("BoxY Value :"+(boxY*0.5f));
             // Debug.Log("BoxY : "+(boxY * 0.5f)/2);
-            boxCollider.offset = new Vector2(boxCollider.offset.x ,(boxY * 0.5f)/2);
+            boxCollider.offset = new Vector2(boxCollider.offset.x, (boxY * 0.5f) / 2);
             boxCollider.size = new Vector2(boxCollider.size.x, boxY * 0.5f);
-        }else if(Input.GetKeyUp(KeyCode.LeftControl)){
-            playerAnimator.SetBool("crouch",false);
-            boxCollider.offset = new Vector2(boxCollider.offset.x,offsetY);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            playerAnimator.SetBool("crouch", false);
+            boxCollider.offset = new Vector2(boxCollider.offset.x, offsetY);
             boxCollider.size = new Vector2(boxCollider.size.x, boxY);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        if (coll.gameObject.tag == "FloorTile"){
+            inAir = false;
         }
     }
 }
