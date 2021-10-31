f8bb0b9e70d67da3f55de383a189d6d1f4e428ab
Jaikishore <jaikishore1997@gmail.com>
2021-11-01 01:19:53 +0530

    Add : LobbySceneConroller, Looby Scene, Lobby to Level transition

---
+++ Assets/Scripts/LobbyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LobbyController : MonoBehaviour
+{
+    public Button playBtn, quitBtn;
+    void Start()
+    {
+        playBtn.onClick.AddListener(LoadGameScene);        
+    }
+
+    private void LoadGameScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
